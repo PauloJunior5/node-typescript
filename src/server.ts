e732000122ee7dfdd1cc90930ec2d05b641872c1
Paulo Junior <pjunior.live@gmail.com>
2021-01-17 14:04:21 -0300

    add - esLint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
+/* eslint-disable no-console */
 import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'Hello World!'
-  });
-})
+app.get('/', (request, response) => response.json({
+  message: 'Hello World!',
+}));
 
 app.listen(3333, () => {
   console.log('🚧 server starts on port 3333!');
-
 });
