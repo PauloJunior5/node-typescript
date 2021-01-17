9ec8b7eb4980f8f7041365a7653be40dc664c095
Paulo Junior <pjunior.live@gmail.com>
2021-01-17 14:13:46 -0300

    add - prettier

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 /* eslint-disable no-console */
-import express from 'express';
-import routes from './routes/index';
+import express from "express";
+import routes from "./routes/index";
 
 const app = express();
 
-app.get('/', (request, response) => response.json({
-  message: 'Hello World!',
-}));
+app.get("/", (request, response) =>
+  response.json({
+    message: "Hello World!",
+  })
+);
 
 app.listen(3333, () => {
-  console.log('🚧 server starts on port 3333!');
+  console.log("🚧 server starts on port 3333!");
 });
