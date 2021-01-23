97612cdb9d6dad16a93063f22e416557a1e92c6a
Paulo Junior <pjunior.live@gmail.com>
2021-01-22 21:04:13 -0300

    creating migrations

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 /* eslint-disable no-console */
-import express from "express";
-import routes from "./routes/index.routes";
+import express from 'express';
+import routes from './routes/index.routes';
+import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.get("/", (request, response) =>
+app.get('/', (request, response) =>
   response.json({
-    message: "Hello World!",
-  })
+    message: 'Hello World!',
+  }),
 );
 
 app.listen(3333, () => {
-  console.log("🚧 server starts on port 3333!");
+  console.log('🚧 server starts on port 3333!');
 });
