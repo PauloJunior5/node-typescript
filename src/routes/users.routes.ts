7a93e0fa7f70c9e9656837799c4325c82ea8dee1
Paulo Junior <pjunior.live@gmail.com>
2021-01-30 16:22:11 -0300

    application commit working

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,7 +3,11 @@ import { Router } from 'express';
 const appointmentsRouter = Router();
 
 appointmentsRouter.post('/', async (request, response) => {
-  return response.send();
+  try {
+    return response.send();
+  } catch (e) {
+    return response.status(400).json({ error: e.message });
+  }
 });
 
 export default appointmentsRouter;
