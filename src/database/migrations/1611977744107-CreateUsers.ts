e43f890da196b3aab78ceec710437e183b6a1a95
Paulo Junior <pjunior.live@gmail.com>
2021-01-30 00:43:08 -0300

    add new migration - Users - part2

--- src/database/migrations/1611977744107-CreateUsers.ts
+++ src/database/migrations/1611977744107-CreateUsers.ts
@@ -4,7 +4,7 @@ export default class CreateUsers1611977744107 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,14 +14,23 @@ export default class CreateUsers1611977744107 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
@@ -29,6 +38,6 @@ export default class CreateUsers1611977744107 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
